3aa23b19f20614f4532e65f37784e8086854cb61
李坤 <likun@smmail.net>
2020-07-21 11:05:00 +0800

    20200721

--- src/components/headertodos.vue
+++ src/components/headertodos.vue
@@ -1,35 +1,42 @@
 <template>
- <header class="header">
-      <h1>todos</h1>
-      <input v-on:keyup.13='add()' value="" v-model="name"
-      class="new-todo" placeholder="What needs to be done?" autofocus>
-    </header>
+  <header class="header">
+    <h1>todos</h1>
+    <input
+      v-on:keyup.13="add()"
+      value
+      v-model="name"
+      class="new-todo"
+      placeholder="What needs to be done?"
+      autofocus
+    />
+  </header>
 </template>
 
 <script>
-
-import { mapState } from 'vuex';
+import { mapState } from "vuex";
 
 export default {
   data() {
     return {
-      name: '',
+      name: ""
     };
   },
+  created() {
+    console.log("日常提交github代码！绿油油的一天开始啦，第一天");
+  },
   computed: {
-    ...mapState(['list']),
+    ...mapState(["list"])
   },
   methods: {
     add() {
-      this.$store.commit('add', {
-        name: this.name,
+      this.$store.commit("add", {
+        name: this.name
       });
-      this.name = '';
-    },
-  },
+      this.name = "";
+    }
+  }
 };
 </script>
 
 <style>
-
 </style>
